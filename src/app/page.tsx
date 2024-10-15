a65ccdd4089e0618b50481e3e3a7f46f77330095
Lee Youl <2yulrang@gmail.com>
2024-10-15 15:49:41 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Examples 야아아아아아아아아아ㅏ
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
