bb2aae9122d3ebd5aa2c812f961c8726ad595718
Lee Youl <2yulrang@gmail.com>
2024-10-15 16:15:48 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <p><strong>dsfaads</strong></p>
+        <p><strong>youl</strong></p>
       </body>
     </html>
   );
